3dca50cfdc512373760ea4294d20e3e5a35a5201
Qasim-Bukhari <qasim.secondry@gmail.com>
2024-12-12 15:51:42 +0500

    Added cors.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { router } from "./routes/shoesRouter.js";
 
 const app = express()
 dotenv.config()
+
+app.use(cors({ origin: "http://localhost:5173" }));
+
 
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to the page where you will the dummy APIs")
